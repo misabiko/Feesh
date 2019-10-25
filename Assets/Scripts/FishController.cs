1b8ec6b6c2a92cf3fe1b5a998882b5f299343771
misabiko <misabiko@gmail.com>
2019-10-25 04:50:43 -0400

    Added neighbor detection + Test spawner + Commented gizmo

--- Assets/Scripts/FishController.cs
+++ Assets/Scripts/FishController.cs
@@ -1,42 +1,62 @@
-﻿using System;
-using UnityEditor;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class FishController : MonoBehaviour {
 	public float speed = 1f;
-	public float viewAngle = 3f;
-	public float rotation = 0f;
-	public Vector3 from = Vector3.left;
+	public float neighborRadius = 1f;
+	//public float viewAngle = 3f;
 
-	public float vBound = 15f;
-	public float hBound = 14f;
+	Collider fishCollider;
 
-	private Color arcColor = new Color(0.5f, 0.5f, 0.5f, 0.5f);
+	const float VBound = 15f;
+	const float HBound = 14f;
 	
-	void Start() {}
+	//private static Color arcColor = new Color(0.5f, 0.5f, 0.5f, 0.5f);
 
-	void Update() {
-		transform.Rotate(Vector3.up, rotation);
-		transform.Translate(Time.deltaTime * speed * transform.forward, Space.World);
-
-		Vector3 pos = transform.position;
-		
-		if (pos.x > hBound)
-			transform.position = new Vector3(-hBound, pos.y, pos.z);
-		else if (transform.position.x < -hBound)
-			transform.position = new Vector3(hBound, pos.y, pos.z);
-
-		if (transform.position.z > vBound)
-			transform.position = new Vector3(pos.x, pos.y, -vBound);
-		else if (transform.position.z < -vBound)
-			transform.position = new Vector3(pos.x, pos.y, vBound);
+	void Start() {
+		fishCollider = GetComponent<Collider>();
 	}
 
-	void OnDrawGizmos() {
+	void Update() {
+		transform.Translate(Time.deltaTime * speed * transform.forward, Space.World);
+
+		checkBounds();
+
+		List<Transform> neighbors = getNeighbors();
+		GetComponentInChildren<Renderer>().material.color = Color.Lerp(Color.white, Color.red, neighbors.Count / 6f);
+	}
+
+	List<Transform> getNeighbors() {
+		List<Transform> neighbors = new List<Transform>();
+		Collider[] colliders = Physics.OverlapSphere(transform.position, neighborRadius);
+		
+		foreach (Collider c in colliders)
+			if (c != fishCollider)
+				neighbors.Add(c.transform);
+
+		return neighbors;
+	}
+
+	void checkBounds() {
+		Vector3 pos = transform.position;
+		
+		if (pos.x > HBound)
+			transform.position = new Vector3(-HBound, pos.y, pos.z);
+		else if (transform.position.x < -HBound)
+			transform.position = new Vector3(HBound, pos.y, pos.z);
+
+		if (transform.position.z > VBound)
+			transform.position = new Vector3(pos.x, pos.y, -VBound);
+		else if (transform.position.z < -VBound)
+			transform.position = new Vector3(pos.x, pos.y, VBound);
+	}
+
+	/*void OnDrawGizmos() {
 		Gizmos.DrawRay(transform.position, transform.forward * 5f);
 		Handles.color = arcColor;
 		Handles.zTest = UnityEngine.Rendering.CompareFunction.LessEqual;
 		Handles.DrawSolidArc(transform.position, Vector3.up, transform.forward, viewAngle / 2, 1f);
 		Handles.DrawSolidArc(transform.position, Vector3.up, transform.forward, -viewAngle / 2, 1f);
-	}
+	}*/
 }
