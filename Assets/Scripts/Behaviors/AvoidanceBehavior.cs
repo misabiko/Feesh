8c7fe3ecb1f8d7524f34b8fc7f72ded67cbbb871
misabiko <misabiko@gmail.com>
2019-10-26 23:17:09 -0400

    Finished the tutorial :D

--- Assets/Scripts/Behaviors/AvoidanceBehavior.cs
+++ Assets/Scripts/Behaviors/AvoidanceBehavior.cs
@@ -1,8 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Flock/Behavior/Avoidance")]
 public class AvoidanceBehavior : FilteredFlockBehavior {
+	public Boolean steered;
+	Vector3 currentVelocity;
+	//time to go from current to next velocity
+	public float agentSmoothTime = 0.5f;
+
+	void Awake() {
+		currentVelocity = new Vector3();
+	}
+	
 	public override Vector3 calculateMove(FlockAgent agent, List<Transform> context, Flock flock) {
 		//if no neighbors, return no adjustment
 		if (context.Count == 0)
@@ -15,13 +25,21 @@ public class AvoidanceBehavior : FilteredFlockBehavior {
 		if (filteredContext.Count == 0)
 			return Vector3.zero;
 
-		foreach (Transform item in filteredContext)
-			if (Vector3.SqrMagnitude(item.position - agent.transform.position) < flock.SquareAvoidanceRadius) {
-				numAvoids++;
-				avoidanceMove += agent.transform.position - item.position;
-			}
+		foreach (Transform item in filteredContext) {
+			Ray ray = new Ray(agent.transform.position, item.position - agent.transform.position);
+			RaycastHit hit;
+			
+			if (item.GetComponent<Collider>().Raycast(ray, out hit, Vector3.SqrMagnitude(item.position - agent.transform.position)))
+				if (hit.distance * hit.distance < flock.SquareAvoidanceRadius) {
+					numAvoids++;
+					avoidanceMove += agent.transform.position - item.position;
+				}
+		}
+
 		if (numAvoids > 0)
 			avoidanceMove /= numAvoids;
+		if (steered)
+			avoidanceMove = Vector3.SmoothDamp(agent.transform.forward, avoidanceMove, ref currentVelocity, agentSmoothTime);
 		
 		return avoidanceMove;
 	}
---
+++ Assets/Scripts/Filters/PhysicsLayerFilter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Filter/Physics Layer")]
+public class PhysicsLayerFilter : ContextFilter {
+    public LayerMask mask;
+    
+    public override List<Transform> filter(FlockAgent agent, List<Transform> original) {
+        List<Transform> filtered = new List<Transform>();
+
+        foreach (Transform item in original)
+            if (mask == (mask | (1 << item.gameObject.layer))) //if (mask == item's layer)
+                filtered.Add(item);
+
+        return filtered;
+    }
+}
